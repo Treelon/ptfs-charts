63e8f236c7a4e363e0a1502d418de4c643fdb353
HotDog640 <Hotdog640derp21@gmail.com>
2024-05-17 23:03:24 +0100

    Rockford Airport (+ reverted RenderAll)
    Old airports from Rockford will be kept in the `_Rockford Old` for historical reasons (the `_` is so it gets passed by the renderer). I know this revision of RenderAll is really crappy, but the website needs it so that all charts are dumped in the one directory. Also the most recent revision broke the imagemagick section.

--- RenderAll.js
+++ RenderAll.js
@@ -9,50 +9,52 @@
 // ****************************************************************************
 
 // @ts-check
-'use strict';
+"use strict";
 
 // Settings
 const settings = {
-  inkscapeCommand: 'E:\\Utils\\Inkscape\\bin\\inkscape.com',
-  magickCommand: 'E:\\Utils\\ImageMagick\\magick.exe',
-  configs: [
-    {
-      dpi: 192,
-      exportDir: '__Results',
-    },
-  ],
+    inkscapeCommand: "C:\\Program Files\\Inkscape\\Bin\\Inkscapecom",
+    magickCommand: "C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\magick.exe",
+    configs: [
+        {
+            dpi: 192,
+            exportDir: "__Results",
+        }
+    ],
 };
 
 ///////////////////////////////////////////////////////////////////////////////
 
-const { spawn, exec } = require('child_process');
-const { statSync, mkdirSync, readdirSync, existsSync } = require('fs');
-const { extname } = require('path');
+const { spawn, exec } = require("child_process");
+const { statSync, mkdirSync, readdirSync } = require("fs");
+const { extname } = require("path");
 const { configs, inkscapeCommand, magickCommand } = settings;
 let cancelled = false;
 let anyKeyExit = false;
 
 // First message
 console.log(
-  'Rendering Charts:\n' +
-    configs
-      .map(
-        ({ dpi, exportDir }) => `    Exporting to ${exportDir}/light\n` + `    with dpi ${dpi}\n`
-      )
-      .join('and\n') +
-    '\nPress "q" at any point to quit.'
+    "Rendering Charts:\n" +
+    configs.map(({ dpi, exportDir }) =>
+        `    Exporting to ${exportDir}/light\n` +
+        `    with dpi ${dpi}\n`
+    ).join("and\n") +
+    "\nPress \"q\" at any point to quit."
 );
 
+
 configs.forEach(({ exportDir }) => {
-  mkdirSync(`./${exportDir}/light`, { recursive: true });
-  mkdirSync(`./${exportDir}/dark`, { recursive: true });
+    mkdirSync(`./${exportDir}/light`, { recursive: true });
+    mkdirSync(`./${exportDir}/dark`, { recursive: true });
 });
 
 /**
  * Filter non-airport folders
  * @param {string} file
- */
-const filter = file => file.charAt(0) !== '_' && statSync(`./${file}`).isDirectory();
+*/
+const filter = (file) =>
+    file.charAt(0) !== "_" && statSync(`./${file}`).isDirectory();
+
 
 // *******************************
 // * Get the paths of all charts *
@@ -62,64 +64,49 @@ const paths = [];
 const charts = [];
 
 // Islands
-readdirSync('.').forEach(island => {
-  if (!filter(island)) return;
+readdirSync(".").forEach((island) => {
+    if (!filter(island)) return;
 
-  // Airports
-  readdirSync(`./${island}`).forEach(airport => {
-    if (!filter(`./${island}/${airport}`)) return;
+    // Airports
+    readdirSync(`./${island}`).forEach((airport) => {
+        if (!filter(`./${island}/${airport}`)) return;
 
-    // Charts
-    readdirSync(`./${island}/${airport}`).forEach(async chart => {
-      if (extname(chart) !== '.svg') return;
+        // Charts
+        readdirSync(`./${island}/${airport}`).forEach(
+            async (chart) => {
+                if (extname(chart) !== ".svg") return;
 
-      charts.push({
-        chartPath: `./${island}/${airport}/${chart}`,
-        chartName: chart,
-        island: island,
-      });
+                paths.push(`${island}/${airport}/${chart}`);
+                charts.push(chart);
+            }
+        );
     });
-  });
 });
+
 
 // ******************************************
 // * Generate the Inkscape CLI instructions *
 // ******************************************
 
 const actions =
-  'export-background-opacity:255; ' +
-  configs
-    .map(
-      ({ dpi, exportDir }) =>
+    "export-background-opacity:255; " +
+    configs.map(({ dpi, exportDir }) => 
         `export-dpi:${dpi}; ` +
-        charts
-          .map((chart, i) => {
-            if (!existsSync(`./__Results/light/${chart.island}`))
-              mkdirSync(`./__Results/light/${chart.island}`);
-            const cn = chart.chartName.split('.')[0];
-            const arrivalsFolder = cn.includes('ILS') || cn.includes('RNAV') ? '/Arrivals' : '';
-            const departuresFolder = cn.includes('DEPARTURE') ? '/Departures' : '';
-            if (arrivalsFolder.length && !existsSync(`./__Results/light/${chart.island}/Arrivals`))
-              mkdirSync(`./__Results/light/${chart.island}/Arrivals`);
-            if (
-              arrivalsFolder.length &&
-              !existsSync(`./__Results/light/${chart.island}/Departures`)
-            )
-              mkdirSync(`./__Results/light/${chart.island}/Departures`);
-
-            return (
-              `file-open:${chart.chartPath}; ` +
-              `export-filename:${exportDir}/light/${chart.island}${arrivalsFolder}${departuresFolder}/${cn}.png; ` +
-              'export-do; ' +
-              'file-close;'
-            );
-          })
-          .join('')
-    )
-    .join('');
+        paths.map((path, i) => 
+            `file-open:${path}; ` +
+            `export-filename:${
+                exportDir}/light/${charts[i].split(".")[0]
+            }.png; ` +
+            "export-do; " +
+            "file-close;"
+        ).join("")
+    ).join("");
 
 // Spawn inkscape
-const Inkscape = spawn(inkscapeCommand, [`--actions=${actions}`]);
+const Inkscape = spawn(
+    inkscapeCommand,
+    [`--actions=${actions}`]
+);
 
 // **********************
 // * Calculate Progress *
@@ -130,17 +117,17 @@ const Inkscape = spawn(inkscapeCommand, [`--actions=${actions}`]);
 // get the percentage complete from the amount of charts times the amount of
 // configs.
 
-// Inkscape.stdout.pipe(process.stdout);
-// Inkscape.stderr.pipe(process.stderr);
+Inkscape.stdout.pipe(process.stdout);
+Inkscape.stderr.pipe(process.stderr);
 
-let childout = '';
+let childout = ""
 const denominator = charts.length * configs.length;
 
-Inkscape.stderr.on('data', chunk => {
-  childout += chunk;
-  const numerator = childout.match(/Background RRGGBBAA/g)?.length || 0;
-  const percent = Math.round((numerator / denominator) * 100);
-  process.stdout.write(`\r\x1b[33m${percent}%\x1b[0m`);
+Inkscape.stderr.on("data", chunk => {
+    childout += chunk;
+    const numerator = childout.match(/Background RRGGBBAA/g)?.length || 0;
+    const percent = Math.round(numerator / denominator * 100);
+    process.stdout.write(`\r\x1b[33m${percent}%\x1b[0m`);
 });
 
 // ***********
@@ -148,77 +135,77 @@ Inkscape.stderr.on('data', chunk => {
 // ***********
 let Magick;
 
-Inkscape.on('exit', () => {
-  configs.forEach(({ exportDir }) => {
-    readdirSync(`./${exportDir}`);
-  });
-
-  // Go through each export dir, find the count of the pngs and add them.
-  const resultCount = configs
-    .map(
-      ({ exportDir }) =>
-        readdirSync(`./${exportDir}/light`).filter(file => extname(file) === '.png').length
-    )
-    .reduce((o, n) => o + n, 0);
-
-  const expectedCount = charts.length * configs.length;
-
-  if (cancelled === true) {
-    console.log(
-      '\n\x1b[31mCancelled rendering charts. Charts which have been ' +
-        'rendered until this point have been saved.\x1b[0m'
-    );
-    console.log('Press any key to exit . . .');
-    anyKeyExit = true;
-  } else if (resultCount < expectedCount) {
-    console.error(
-      '\n\x1b[31mThere are less pngs in the export directories then ' +
-        `there expected. There should be ${expectedCount} charts but ` +
-        `only ${resultCount} .png files were found. Ensure all charts ` +
-        'were exported correctly.\x1b[0m'
-    );
-    console.log('Press any key to exit . . .');
-    anyKeyExit = true;
-  } else {
-    console.log(
-      '\n\x1b[32mDone Inkscape!\x1b[0m\n' +
-        'Generating dark mode:\n' +
-        configs.map(({ exportDir }) => `    Exporting to ${exportDir}/dark\n`).join('and\n')
-    );
-
-    const execStr = configs
-      .map(
-        ({ exportDir }) =>
-          `"${magickCommand}" convert -modulate 100,100,0 -channel RGB ` +
-          `-negate "./${exportDir}/light/*.png" -verbose -set ` +
-          `filename:original %t "./${exportDir}/dark/%[filename:original]` +
-          '.png"'
-      )
-      .join(' & ');
-
-    Magick = exec(execStr);
-
-    let childout = '';
-    Magick.stdout.on('data', chunk => {
-      childout += chunk;
-      const numerator = childout.match(/sRGB/g)?.length || 0;
-      const percent = Math.round((numerator / denominator) * 100);
-      process.stdout.write(`\r\x1b[33m${percent}%\x1b[0m`);
+Inkscape.on("exit", () => {
+    configs.forEach(({ exportDir }) => {
+        readdirSync(`./${exportDir}`);
     });
 
-    Magick.on('exit', () => {
-      console.log('Press any key to exit . . .');
-      anyKeyExit = true;
-    });
-  }
+    // Go through each export dir, find the count of the pngs and add them.
+    const resultCount = configs.map(({ exportDir }) => 
+        readdirSync(`./${exportDir}/light`).filter(
+            file => extname(file) === ".png"
+        ).length
+    ).reduce((o, n) => o + n, 0);
+
+    const expectedCount = charts.length * configs.length;
+
+    if (cancelled === true) {
+        console.log(
+            "\n\x1b[31mCancelled rendering charts. Charts which have been " +
+            "rendered until this point have been saved.\x1b[0m"
+        );
+        console.log("Press any key to exit . . .");
+        anyKeyExit = true;
+    }
+    else if (resultCount < expectedCount) {
+        console.error(
+            "\n\x1b[31mThere are less pngs in the export directories then " +
+            `there expected. There should be ${expectedCount} charts but ` +
+            `only ${resultCount} .png files were found. Ensure all charts ` +
+            "were exported correctly.\x1b[0m"
+        );
+        console.log("Press any key to exit . . .");
+        anyKeyExit = true;
+    }
+    else {
+        console.log(
+            "\n\x1b[32mDone Inkscape!\x1b[0m\n" +
+            "Generating dark mode:\n" +
+            configs.map(({ exportDir }) =>
+                `    Exporting to ${exportDir}/dark\n`
+            ).join("and\n")
+        );
+
+        const execStr = configs.map(({ exportDir }) => 
+            `"${magickCommand}" convert -modulate 100,100,0 -channel RGB ` +
+            `-negate "./${exportDir}/light/*.png" -verbose -set ` +
+            `filename:original %t "./${exportDir}/dark/%[filename:original]` +
+            '.png"'
+        ).join(" & ");
+
+        Magick = exec(execStr);
+
+        let childout = "";
+        Magick.stdout.on("data", chunk => {
+            childout += chunk;
+            const numerator = childout.match(/sRGB/g)?.length || 0;
+            const percent = Math.round(numerator / denominator * 100);
+            process.stdout.write(`\r\x1b[33m${percent}%\x1b[0m`);
+        });
+
+        Magick.on("exit", () => {
+            console.log("Press any key to exit . . .");
+            anyKeyExit = true;
+        });
+    };
 });
 
-process.stdin.setRawMode(true).on('data', chunk => {
-  if (anyKeyExit) return process.stdin.destroy();
+process.stdin.setRawMode(true).on("data", chunk => {
+    if (anyKeyExit) return process.stdin.destroy();
 
-  if (chunk.toString().match('q')) {
-    cancelled = true;
-    Inkscape.kill();
-    Magick.kill?.();
-  }
+    if (chunk.toString().match("q")) {
+        cancelled = true;
+        Inkscape.kill();
+        Magick.kill?.();
+    };
 });
